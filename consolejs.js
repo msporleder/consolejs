ceba5833520c837c9b5af3a9ad12c219c177f593
Matthew Sporleder <msporleder@gmail.com>
2013-06-07 14:43:19 -0400

    consolejs is now a nice little module and index.html demos how to use it

--- consolejs.js
+++ consolejs.js
@@ -1,20 +1,32 @@
 "use strict";
 
-var Consolejs = function (win, winstyle) {
-  var inp, inptxt, button, respond, divwin, sub;
-  divwin = '<div id="input">'
-        + '<form onsubmit="return false;">'
-        + '<input id="inp" autofocus class="black" autocomplete="off"></input>'
-        + '<br />'
-        + '<input id="sub" type="submit" value=">" class="black"></input>'
-        + '</form></div>';
-  win.innerHTML = divwin;
-  inp = document.getElementById("inp");
-  inptxt = document.getElementById("tmp");
-  sub = document.getElementById("sub");
-  sub.addEventListener('click', function () { cons.button(cons, rep); }, false);
-  return {
-    button: function (self, cb) {
+var Consolejs = function (win, cb, winstyle) {
+  var inp, inptxt, outwin, sub, form, button, respond;
+  winstyle = getComputedStyle(win);
+  outwin = document.createElement("div");
+  outwin.setAttribute("id", "outwin");
+  outwin.setAttribute("class", 'consolejs');
+  outwin.style.width = winstyle.getPropertyValue("width");
+  outwin.style.height = winstyle.getPropertyValue("height");;
+  inp = document.createElement("input");
+  inp.setAttribute("id", 'inp');
+  inp.setAttribute("autofocus", 'true');
+  inp.setAttribute("autocomplete", 'off');
+  inp.setAttribute("class", 'consolejs');
+  inp.style.borderTopWidth = '1px';
+  inp.style.borderLeftWidth = '0px';
+  inp.style.borderRightWidth = '0px';
+  inp.style.width = outwin.style.width;
+  form = document.createElement('form');
+  form.setAttribute("onsubmit", 'return false;');
+  form.appendChild(inp);
+  //kill the existing content and replace with our own
+  win.innerHTML = '';
+  win.appendChild(outwin);
+  win.appendChild(form);
+  form.addEventListener('submit', function () { button(cb); }, false);
+
+  var button = function (cb) {
 // get value and escape some html
       var inptc, pos;
       inptc = inp.value.replace(/&/g, '&amp;')
@@ -22,17 +34,18 @@ var Consolejs = function (win, winstyle) {
                   .replace(/>/g, '&gt;')
                   .replace(/"/g, '&quot;')
                   .replace(/'/g, '&apos;');
-      // window.alert(inptc);
-      win.insertAdjacentHTML('beforeend',
+      //window.alert(inptc);
+      outwin.insertAdjacentHTML('beforeend',
           "<span class=\"usertxt\">> "
           + inptc
           + "<br /></span>");
-      pos = win.scrollTop;
-      win.scrollTop = pos + win.clientHeight;
+      pos = outwin.scrollTop;
+      outwin.scrollTop = pos + outwin.clientHeight;
       inp.value = '';
-      self.respond(inptc, cb);
-    },
-    respond: function (inp, cb) {
+      //self.respond(inptc, cb);
+      respond(inptc, cb);
+    };
+    var respond = function (inp, cb) {
       var pos, resp;
       if (typeof cb !== 'undefined') {
         resp = cb(inp);
@@ -40,25 +53,12 @@ var Consolejs = function (win, winstyle) {
         resp = inp;
       }
       //    + win.lastChild.textContent
-      win.insertAdjacentHTML('beforeend',
+      outwin.insertAdjacentHTML('beforeend',
           "<span class=\"bottxt\">"
           + resp
           + "<br /></span>");
-      pos = win.scrollTop;
-      win.scrollTop = pos + win.clientHeight;
-    }
-  };
+      pos = outwin.scrollTop;
+      outwin.scrollTop = pos + outwin.clientHeight;
+    };
 };
 
-var rep = function(inp) {
-  return "foo: " + inp;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-// scroll to bottom of div
-// init stuff
-  var sub, cons, win;
-  // sub = document.getElementById("sub");
-  win  = document.getElementById("cons");
-  cons = new Consolejs(win);
-});
